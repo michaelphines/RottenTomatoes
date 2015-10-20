d5f387fe2c4f1acd912b275b58f3bfb9ea201ebe
Michael Hines <mhines@groupon.com>
2015-10-20 17:07:10 -0500

    Initial Commit

---
+++ Rotten Tomatoes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rotten Tomatoes
+//
+//  Created by Michael Hines on 10/20/15.
+//  Copyright © 2015 Michael Hines. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
